e61ae339ac1aa7bbf48bceb303e14741b00a334e
Filippo Valle <filippo.valle@edu.unito.it>
2020-08-17 18:08:31 +0200

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="topicpy", # Replace with your own username
-    version="1.1.1",
+    version="1.1.5",
     author="Filippo Valle",
     author_email="filippo.valle@unito.it",
     description="Package to extract information from topic models.",
@@ -18,5 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires = ["tensorflow","gseapy","seaborn", "sklearn", "pandarallel","tensorflow_probability", "numpy", "matplotlib", "pandas"],
     python_requires='>=3.6',
 )
--- test/test.py
+++ test/test.py
@@ -1,2 +1,6 @@
 import unittest
 
+class TopicTest(unittest.TestCase):
+	def test_import(self):
+		import topicpy
+		self.assertEqual("1.1.5",topicpy.__version)
