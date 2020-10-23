1724c282ed4b375eab9424069dad7fde8a764aa5
Filippo Valle <filippo.valle@edu.unito.it>
2020-10-23 11:16:29 +0200

    fix type

--- topicpy/converter/converter.py
+++ topicpy/converter/converter.py
@@ -25,7 +25,7 @@ import pandas as pd
 url = "https://www.genenames.org/cgi-bin/download/custom?col=gd_app_sym&col=md_ensembl_id&status=Approved&status=Entry%20Withdrawn&hgnc_dbtag=on&order_by=gd_app_sym_sort&format=text&submit=submit"
 df_conversion = pd.read_csv(url, sep="\t", index_col=1).dropna()
 
-def convert_list_to_sybmols(ensgs -> list):
+def convert_list_to_sybmols(ensgs: list) -> list:
 	"""
 	is converts a list of ENSG to gene names
 
@@ -33,7 +33,7 @@ def convert_list_to_sybmols(ensgs -> list):
 	"""
 	return [df_conversion.at[g,"Approved symbol"]  if g in df_conversion.index else g for g in ensgs]
 
-def convert_list_to_ensg(symbols -> list):
+def convert_list_to_ensg(symbols: list) -> list:
 	"""
 	it converts a list of gene names to ENSG identifiers
 
