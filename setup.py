27c364b2e0fa53e735a232358d8108ea99ca0931
Filippo Valle <filippo.valle@edu.unito.it>
2021-02-02 15:59:09 +0100

    update dependencies

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="topicpy", # Replace with your own username
-    version="1.1.10",
+    name="topicpy",
+    version="0.0.3",
     author="Filippo Valle",
     author_email="filippo.valle@unito.it",
     description="Package to extract information from topic models.",
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires = ["tensorflow","gseapy","seaborn", "sklearn", "pandarallel","tensorflow_probability", "numpy", "matplotlib", "pandas"],
+    install_requires = ["tensorflow","gseapy","seaborn", "sklearn","tensorflow_probability", "numpy", "matplotlib", "pandas"],
     python_requires='>=3.6',
 )
